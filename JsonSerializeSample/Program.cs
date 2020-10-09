dc58e33a143156cfad988c98882508c1a66381e0
takunology <m15025@g.metro-cit.ac.jp>
2020-10-09 14:48:39 +0900

    build

---
+++ JsonSerializeSample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace JsonSerializeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Json serialize.");
+            Serialize();
+        }
+
+        static void Serialize()
+        {
+            var sampleData = new SampleData()
+            {
+                Body = new Body()
+                {
+                    Origin = new Origin() { type = "player" },
+                    commandLine = "time set 0",
+                    version = "1"
+                },
+
+                Header = new Header()
+                {
+                    requestId = "e608eba7-13cb-48f9-b8d5-139088cc7cd8",
+                    messagePurpose = "commandRequest",
+                    version = "1",
+                    messageType = "commandRequest"
+                }
+            };
+
+            var serializer = new DataContractJsonSerializer(typeof(SampleData));
+            var ms = new MemoryStream();
+            serializer.WriteObject(ms, sampleData);
+            Console.WriteLine(Encoding.UTF8.GetString(ms.ToArray()));
+        }
+    }
+}
---
+++ JsonSerializeSample/SampleData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace JsonSerializeSample
+{
+    [DataContract]
+    public class SampleData
+    {
+        [DataMember(Name = "body")]
+        public Body Body { get; set; }
+        [DataMember(Name = "header")]
+        public Header Header { get; set; }
+    }
+
+    [DataContract]
+    public class Origin
+    {
+        [DataMember(Name = "type")]
+        public string type { get; set; }
+    }
+
+    [DataContract]
+    public class Body
+    {
+        [DataMember(Name = "origin")]
+        public Origin Origin { get; set; }
+
+        [DataMember(Name = "commandLine")]
+        public string commandLine { get; set; }
+
+        [DataMember(Name = "version")]
+        public string version { get; set; }
+    }
+
+    [DataContract]
+    public class Header
+    {
+        [DataMember(Name = "requestId")]
+        public string requestId { get; set; }
+
+        [DataMember(Name = "messagePurpose")]
+        public string messagePurpose { get; set; }
+
+        [DataMember(Name = "version")]
+        public string version { get; set; }
+
+        [DataMember(Name = "messageType")]
+        public string messageType { get; set; }
+    }
+}
